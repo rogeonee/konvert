268336ae957552eb29ed590dcdfc0515b7239263
Egor <egormaxb@gmail.com>
2024-10-26 22:40:36 -0700

    fix: tap through in selects on mobile

--- components/select-format.tsx
+++ components/select-format.tsx
@@ -38,7 +38,12 @@ const SelectFormat: React.FC<SelectFormatProps> = ({ control, name }) => {
           >
             <SelectValue placeholder="Convert to" defaultValue="jpg" />
           </SelectTrigger>
-          <SelectContent>
+          <SelectContent
+            ref={(ref) => {
+              if (!ref) return;
+              ref.ontouchstart = (e) => e.preventDefault();
+            }}
+          >
             {formats.map((format) => (
               <SelectItem key={format.value} value={format.value}>
                 <p className="font-medium text-foreground">{format.label}</p>
